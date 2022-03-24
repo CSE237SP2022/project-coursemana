e4b4a43cb8a6299f57624eb358ac46b14e4f42dd
Pascal-Yang <pascal@wustl.edu>
2022-03-23 19:26:28 -0500

    some tests implemented

--- src/test/Course_test.java
+++ src/test/Course_test.java
@@ -13,9 +13,21 @@ public class Course_test {
 	}
 
 	@Test
-	public void test_constructor() {
+	public void test_constructor1() {
 		final String course_name = "Computer Science";
 		Course demo_course_obj = new Course();
 	}
+	
+	
+	@Test
+	public void test_set_get_name() {
+		Course demo_course_obj = new Course();
+		demo_course_obj.setName("cse237");
+		assertEquals(demo_course_obj.getName(), "cse237");
+		demo_course_obj.setName("cse131");
+		assertEquals(demo_course_obj.getName(), "cse131");
+	}
+	
+	
 
 }
