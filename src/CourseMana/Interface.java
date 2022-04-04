eb88c86b0e20504841ae35671601018ae8f49ebe
MuhanYu <muhan.y@wustl.edu>
2022-04-04 14:30:01 -0500

    usage message method done

--- src/CourseMana/Interface.java
+++ src/CourseMana/Interface.java
@@ -1,31 +1,61 @@
 package CourseMana;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Interface {
 	
-	public static void run() {
-		Scanner scanner = new Scanner(System.in); 
-		String input = scanner.nextLine();
+	private List<String> listOfCommands;
+	
+	public Interface() {
+		this.listOfCommands = new LinkedList<String>();
+		this.listOfCommands.add("add student");
+		this.listOfCommands.add("add teacher");
+		this.listOfCommands.add("add student");
+	}
+	
+	
+	public void printUsageMessage() {
+		System.out.println("This program accepts the following commands");
+		for (String command : listOfCommands) {
+			System.out.println("<" + command + ">");
+		}
+	}
+	
+	public void executeCommand(String input) {
 		switch(input) {
-		case "Add Student":
+		case "add student":
 			addStudent();
 			break;
-		case "Add Teacher":
+		case "add teacher":
 			addTeacher();
 			break;
-		case "Add Course":
+		case "add course":
 			addCourse();
 			break;
 		default:
 			printUsageMessage();
+			break;S
+		}
+	}
+	
+	public void run() {
+		Scanner scanner = new Scanner(System.in); 
+		String input = scanner.nextLine();
+		
+		for (String command : listOfCommands) {
+			if (input.equals(command)) {
+				executeCommand(input);
+			}
 		}
 	}
 	
 	
 	public static void main(String[] args) {
+		Interface i = new Interface();
 		while(true) {
-			run();
+			i.run();
 		}
 	}
 
