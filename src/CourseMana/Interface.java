9dae7041e909e292d1b6451a0bf0aec3c660b3da
Pascal-Yang <pascal@wustl.edu>
2022-04-06 15:54:42 -0500

    done_addCourse

--- src/CourseMana/Interface.java
+++ src/CourseMana/Interface.java
@@ -1,10 +1,16 @@
 package CourseMana;
 
+
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.concurrent.CompletableFuture.AsynchronousCompletionTask;
+
+import javax.crypto.AEADBadTagException;
+
+import CourseMana.Student;
 
 public class Interface {
 	
@@ -17,7 +23,8 @@ public class Interface {
 		this.listOfCommands = new LinkedList<String>();
 		this.listOfCommands.add("add student");
 		this.listOfCommands.add("add teacher");
-		this.listOfCommands.add("add student");
+		this.listOfCommands.add("add course");
+		this.listOfCommands.add("list course");
 		
 		this.idToCourse = new HashMap<String, Course>();
 		this.idToStudent = new HashMap<String, Student>();
@@ -27,22 +34,49 @@ public class Interface {
 	
 	
 	public void printUsageMessage() {
-		System.out.println("This program accepts the following commands");
+		System.out.println("Cannot recognize user input.This program accepts the following commands: ");
 		for (String command : listOfCommands) {
 			System.out.println("<" + command + ">");
 		}
 	}
 	
+	// add new course to the system
+	private void addCourse() {
+		System.out.println("Hello From addCourse()");
+		String courseName = askStringInput("Please Enter A Course Name: ");
+		int courseSize = askPosIntInput("Please Enter The Size of the Course: ");
+		String teacherId = askStringInput("Please Enter Teacher ID: ");
+		
+		Course newCourse = new Course(courseName, courseSize, idToTeacher.get(teacherId));
+		idToCourse.put(courseName, newCourse);
+		
+		System.out.println(courseName + " of size " + courseSize + " is added. ");
+		
+	}
+	
+	// list all courses in the system
+	private void listCourse() {
+		System.out.println("Hello From listCourse()");
+	}
+		
+	
 	public void executeCommand(String input) {
 		switch(input) {
 		case "add student":
-			addStudent();
+			System.out.println("Calling addStudent()...");
+			//addStudent();
 			break;
 		case "add teacher":
-			addTeacher();
+			System.out.println("Calling addTeacher()...");
+			//addTeacher();
 			break;
 		case "add course":
+			System.out.println("Calling addCourse()...");
 			addCourse();
+			break;
+		case "list course":
+			System.out.println("Calling listCourse()...");
+			listCourse();
 			break;
 		default:
 			printUsageMessage();
@@ -52,17 +86,39 @@ public class Interface {
 	
 	public void run() {
 		Scanner scanner = new Scanner(System.in); 
+		System.out.println("\nEnter Command:");
 		String input = scanner.nextLine();
 		
-		for (String command : listOfCommands) {
-			if (input.equals(command)) {
-				executeCommand(input);
-			}
+		executeCommand(input);
+		
+	}
+	
+	
+	//ask for positive integer input from the user
+	private int askPosIntInput(String prompt) {
+		Scanner scanner = new Scanner(System.in); 
+		System.out.println(prompt);
+		int input = scanner.nextInt();
+		while(input<0) {
+			System.out.println("Please enter a positive integer.");
+			input = scanner.nextInt();
 		}
+		return input;
+	}
+	
+	// ask for string input from the user
+	private String askStringInput(String prompt) {
+		Scanner scanner = new Scanner(System.in); 
+		System.out.println(prompt);
+		String input = scanner.nextLine();
+		return input;
 	}
 	
 	
 	public static void main(String[] args) {
+		
+		System.out.println("Welcome to CourseMana :)");
+		
 		Interface i = new Interface();
 		while(true) {
 			i.run();
